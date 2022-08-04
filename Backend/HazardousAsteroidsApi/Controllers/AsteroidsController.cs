a47a8523589b883f742486a7517ef9b0531eac37
R. Beltran <rbeltran8000@gmail.com>
2022-08-04 19:40:23 +0200

    initial commit

---
+++ Backend/HazardousAsteroidsApi/Controllers/AsteroidsController.cs
@@ -0,0 +1,22 @@
+﻿using HazardousAsteroidsApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HazardousAsteroidsApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AsteroidsController : ControllerBase
+{
+    private readonly ILogger<AsteroidsController> _logger;
+
+    public AsteroidsController(ILogger<AsteroidsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Asteroid> Get()
+    {
+        return Array.Empty<Asteroid>();
+    }
+}
---
+++ Backend/HazardousAsteroidsApi/Model/Asteroid.cs
@@ -0,0 +1,14 @@
+﻿namespace HazardousAsteroidsApi.Model;
+
+public class Asteroid
+{
+    public string? Name { get; set; }
+
+    public float Diameter { get; set; }
+
+    public float Speed { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string? Planet { get; set; }
+}
