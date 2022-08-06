516435b0d3e2d5374924938a90a4f2e111ddb189
R. Beltran <rbeltran8000@gmail.com>
2022-08-06 08:41:50 +0200

    add tailwind css + replace "preinstalled" app page with a basic one

---
+++ Frontend/HazardousAsteroidsUI/client-app/src/lib/date-utils.ts
@@ -0,0 +1,11 @@
+﻿
+// only-valid-in-js // const addDays = (d: Date, numDays: number) => (new Date()).setDate(d.getDate() + numDays);
+
+function addDays(d: Date, numDays: number): Date {
+    const res = new Date()
+    res.setDate(d.getDate() + numDays)
+
+    return res
+}
+
+export { addDays }
---
+++ Frontend/HazardousAsteroidsUI/client-app/src/pages/HazardousAsteroidsPage/HazardousAsteroidsPage.tsx
@@ -0,0 +1,45 @@
+﻿import React from 'react'
+import { addDays } from '../../lib/date-utils'
+
+export interface AsteroidsApiResponse {
+  pageIndex: number
+  pageSize: number
+  count: number
+  data: any
+}
+
+export interface AsteroidsApiRequest {
+  startDate: Date
+  endDate: Date
+  planet?: string
+}
+
+type HazardousAsteroidsPageState = AsteroidsApiResponse & AsteroidsApiRequest
+
+export class HazardousAsteroidsPage extends React.Component<{}, HazardousAsteroidsPageState> {
+
+  state = {
+    startDate: new Date(),
+    endDate: addDays(new Date(), 7),
+    planet: 'Earth',
+    pageIndex: 0,
+    pageSize: 10,
+    count: 0,
+    data: []
+  }
+
+  componentDidMount() {
+    // TODO
+  }
+
+  render() {
+
+    return (
+      <article className='w-full p-0 sm:p-4 lg:max-w-4xl mx-auto bg-white sm:rounded-xl shadow-lg items-center space-y-4'>
+        HAZARDOUS ASTEROIDS PAGE CONTENT
+      </article>
+    )
+  }
+}
+
+export default HazardousAsteroidsPage
